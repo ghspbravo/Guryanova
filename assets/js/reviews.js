ee768b2147784dc7e9e5e49b92dc9a6307ff7072
Наталья Лозинская <lozinskaya99@bk.ru>
2018-08-05 20:21:19 +0500

    beta_scroll

--- assets/js/reviews.js
+++ assets/js/reviews.js
@@ -34,3 +34,23 @@ let showAnswer = (question, answer) => {
 	answerWrapper[1].innerHTML = question
 	answerWrapper[2].innerHTML = answer
 }
+var scrollInterval = setInterval(scroll_in_div, 5000); // задали время промотки
+var scroll_div = document.querySelector('#reviews'); 
+// scroll_div.scrollTo({ //вверх
+    // top: 2000,
+    // behavior: "smooth"
+// });
+// scroll_div.scrollTo({ //вниз
+    // top: 0,
+    // behavior: "smooth"
+// });
+function scroll_in_div (){
+	scroll_div.scrollTo({ //вверх
+    top: 2000,
+    behavior: "smooth"
+});
+	scroll_div.scrollTo({ //вниз
+		top: 0,
+		behavior: "smooth"
+});
+}
