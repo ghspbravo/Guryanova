a58c8623083c8f87108307965a08ce26b7a5f01a
ghspbravo <32223917+ghspbravo@users.noreply.github.com>
2018-08-05 21:00:27 +0500

    scroll fixes

--- assets/js/reviews.js
+++ assets/js/reviews.js
@@ -34,25 +34,51 @@ let showAnswer = (question, answer) => {
 	answerWrapper[1].innerHTML = question
 	answerWrapper[2].innerHTML = answer
 }
-var scrollInterval = setInterval(scroll_in_div, 5000);
-var scroll_div = document.querySelector('#reviews'); 
-function scroll_in_div (){
-	scroll_div.scrollTo({ //вверх
-    top: 2000,
-    behavior: "smooth"
-});
-	scroll_div.scrollTo({ //вниз
-		top: 0,
-		behavior: "smooth"
-});
+var scrollInterval = setInterval(scroll_in_div, 100);
+var scroll_div = document.querySelector('#reviews');
+// function scroll_in_div() {
+// 	scroll_div.scrollTo({ //вверх
+// 		top: 2000,
+// 		behavior: "smooth"
+// 	});
+// 	scroll_div.scrollTo({ //вниз
+// 		top: 0,
+// 		behavior: "smooth"
+// 	});
+// }
+
+let scrollDirection = 'bottom';
+function scroll_in_div() {
+	let currentScroll = scroll_div.scrollTop
+	scroll_div.scrollHeight - currentScroll - scroll_div.clientHeight < 0.1 && scrollDirection === 'bottom'
+	? scrollDirection = 'top'
+	: scroll_div.scrollHeight - currentScroll >= scroll_div.scrollHeight && scrollDirection === 'top'
+		? scrollDirection = 'bottom'
+		: false
+
+	switch (scrollDirection) {
+		case 'bottom':
+			scroll_div.scrollTo({
+				top: currentScroll + 10,
+				behavior: "smooth"
+			});
+			break;
+		case 'top':
+			scroll_div.scrollTo({
+				top: currentScroll - 10,
+				behavior: "smooth"
+			});
+			break;
+
+		default:
+			break;
+	}
 }
 
-scroll_div.addEventListener("mouseover",function()
-{
+scroll_div.addEventListener("mouseover", function () {
 	clearInterval(scrollInterval);
-}); 
+});
 
-scroll_div.addEventListener("mouseout",function()
-{
-	scrollInterval = setInterval(scroll_in_div, 5000);
+scroll_div.addEventListener("mouseout", function () {
+	scrollInterval = setInterval(scroll_in_div, 100);
 }); 
