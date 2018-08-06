22cc40a65601a0113364b69751e718bdfcbba0b1
ghspbravo <32223917+ghspbravo@users.noreply.github.com>
2018-08-06 13:46:08 +0500

    model size js styles

--- assets/js/reviews.js
+++ assets/js/reviews.js
@@ -1,32 +1,14 @@
 window.onload = function () {
-	// var question_one = document.querySelector('#question_one');
-	// var answer_one = document.querySelector('#answer_one');
-	// var question_two = document.querySelector('#question_two');
-	// var answer_two = document.querySelector('#answer_two');
-	// var question_three = document.querySelector('#question_three');
-	// var answer_three = document.querySelector('#answer_three');
-	// var select_question = answer_one;
-	// question_one.onclick = function () {
-	// 	select_question.className = ('faq-answer answer');//поменяли класс
-	// 	answer_one.className = ('faq-answer answer showing');//поменяли класс
-	// 	select_question = answer_one;
-	// }
-	// question_two.onclick = function () {
-	// 	select_question.className = ('faq-answer answer');//поменяли класс
-	// 	answer_two.className = ('faq-answer answer showing');//поменяли класс
-	// 	select_question = answer_two;
-	// }
-	// question_three.onclick = function () {
-	// 	select_question.className = ('faq-answer answer');//поменяли класс
-	// 	answer_three.className = ('faq-answer answer showing');//поменяли класс
-	// 	select_question = answer_three;
-	// }
+
 	document.querySelectorAll('.question').forEach(question => {
 		let questionTitle = question.dataset.question
 		let answer = question.dataset.answer
 
 		question.onclick = () => showAnswer(questionTitle, answer)
 	})
+
+	document.querySelector('.model').style.height = `${document.querySelector('#banner').clientHeight}px`
+	document.querySelector('.model').style.transform = `translateY(-${document.querySelector('#banner').clientHeight * 0.45}px)`
 }
 
 let showAnswer = (question, answer) => {
@@ -36,16 +18,6 @@ let showAnswer = (question, answer) => {
 }
 var scrollInterval = setInterval(scroll_in_div, 100);
 var scroll_div = document.querySelector('#reviews');
-// function scroll_in_div() {
-// 	scroll_div.scrollTo({ //вверх
-// 		top: 2000,
-// 		behavior: "smooth"
-// 	});
-// 	scroll_div.scrollTo({ //вниз
-// 		top: 0,
-// 		behavior: "smooth"
-// 	});
-// }
 
 let scrollDirection = 'bottom';
 function scroll_in_div() {
